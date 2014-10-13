efbc8bede8c066a20b19ffd8a49ca465597ffc5f
Daniel DeGroff <djdegroff@gmail.com>
2014-10-13 12:27:51 -0600

    Fix line returns in *.groovy parts and fix IML file to be JAVA module.

--- src/main/groovy/org/savantbuild/plugin/spock/SpockRunner.groovy
+++ src/main/groovy/org/savantbuild/plugin/spock/SpockRunner.groovy
@@ -1,1 +1,62 @@
-/* * Copyright (c) 2014, Inversoft Inc., All Rights Reserved * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *   http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, * software distributed under the License is distributed on an * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, * either express or implied. See the License for the specific * language governing permissions and limitations under the License. */package org.savantbuild.plugin.spockimport java.nio.file.Pathimport org.savantbuild.domain.Projectimport org.savantbuild.lang.Classpathimport org.savantbuild.output.Output/** * @author Daniel DeGroff */class SpockRunner {  Path groovyPath  Output output  Project project  SpockSettings settings  int doRun(Classpath classpath, SpockSuite suite) {    if (!suite.initialized) {      throw new IllegalStateException("SpockSuite must be initialized prior to calling doRun() on the SpockRunner.")    }    int result;    suite.tests.each() {      output.info("[Spec] ${it}")      String command = "${groovyPath} ${settings.jvmArguments} ${classpath.toString("-cp ")} ${it}"      output.debug(" Running command: [%s]", command)      Process process = command.execute(null, project.directory.toFile())      process.consumeProcessOutput(System.out, System.err)      result = process.waitFor()      if (result != 0) {        return result;      }    }    return result  }}
+/*
+ * Copyright (c) 2014, Inversoft Inc., All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.savantbuild.plugin.spock
+
+import java.nio.file.Path
+
+import org.savantbuild.domain.Project
+import org.savantbuild.lang.Classpath
+import org.savantbuild.output.Output
+
+/**
+ * @author Daniel DeGroff
+ */
+class SpockRunner {
+
+  Path groovyPath
+
+  Output output
+
+  Project project
+
+  SpockSettings settings
+
+  int doRun(Classpath classpath, SpockSuite suite) {
+
+    if (!suite.initialized) {
+      throw new IllegalStateException("SpockSuite must be initialized prior to calling doRun() on the SpockRunner.")
+    }
+
+    int result;
+    suite.tests.each() {
+
+      output.info("[Spec] ${it}")
+
+      String command = "${groovyPath} ${settings.jvmArguments} ${classpath.toString("-cp ")} ${it}"
+      output.debug(" Running command: [%s]", command)
+
+      Process process = command.execute(null, project.directory.toFile())
+      process.consumeProcessOutput(System.out, System.err)
+
+      result = process.waitFor()
+      if (result != 0) {
+        return result;
+      }
+
+    }
+    return result
+  }
+}
--- src/main/groovy/org/savantbuild/plugin/spock/SpockSuite.groovy
+++ src/main/groovy/org/savantbuild/plugin/spock/SpockSuite.groovy
@@ -1,1 +1,63 @@
-/* * Copyright (c) 2014, Inversoft Inc., All Rights Reserved * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *   http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, * software distributed under the License is distributed on an * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, * either express or implied. See the License for the specific * language governing permissions and limitations under the License. */package org.savantbuild.plugin.spockimport java.nio.file.FileVisitResultimport java.nio.file.Filesimport java.nio.file.Pathimport java.nio.file.Pathsimport java.nio.file.SimpleFileVisitorimport java.nio.file.attribute.BasicFileAttributesimport org.savantbuild.output.Output/** * @author Daniel DeGroff */class SpockSuite {  Set<String> tests = new TreeSet<>()  boolean initialized  Output output  String spockExtension = "Spec.groovy"  String sourceTestDirectory = "src/test/groovy"  private findSpecifications() {    Files.walkFileTree( Paths.get(sourceTestDirectory), new SimpleFileVisitor<Path>() {      @Override      public FileVisitResult visitFile(Path filePath, BasicFileAttributes attributes) {        if (filePath != null) {          output.debug(filePath.fileName.toString());          if (filePath.fileName.toString().endsWith(spockExtension)) {            output.debug(filePath.toString())            tests.add(filePath.toString())          }        }        return FileVisitResult.CONTINUE      }    })  }  void initialize() {    findSpecifications()    initialized = true  }}
+/*
+ * Copyright (c) 2014, Inversoft Inc., All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.savantbuild.plugin.spock
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+
+import org.savantbuild.output.Output
+
+/**
+ * @author Daniel DeGroff
+ */
+class SpockSuite {
+
+  Set<String> tests = new TreeSet<>()
+
+  boolean initialized
+
+  Output output
+
+  String spockExtension = "Spec.groovy"
+
+  String sourceTestDirectory = "src/test/groovy"
+
+  private findSpecifications() {
+    Files.walkFileTree( Paths.get(sourceTestDirectory), new SimpleFileVisitor<Path>() {
+
+      @Override
+      public FileVisitResult visitFile(Path filePath, BasicFileAttributes attributes) {
+        if (filePath != null) {
+          output.debug(filePath.fileName.toString());
+          if (filePath.fileName.toString().endsWith(spockExtension)) {
+            output.debug(filePath.toString())
+            tests.add(filePath.toString())
+          }
+        }
+        return FileVisitResult.CONTINUE
+      }
+    })
+  }
+
+  void initialize() {
+    findSpecifications()
+    initialized = true
+  }
+
+}
